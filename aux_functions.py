01668c1c90d73eeb67daf66e51730060082e8a79
Dongsub Lee <dongsub93@gmail.com>
2021-03-30 15:48:26 +0900

    Update aux_functions.py

--- aux_functions.py
+++ aux_functions.py
@@ -12,9 +12,11 @@ def drawHistory(data:pd.DataFrame):
     _prices= data.iloc[:,range(2*_nAsset+2,3*_nAsset+2)].values
     #_assets= data.iloc[:,range(2,_nAsset+2)].values
     _fig, _ax = plt.subplots(1,1,sharex=True,figsize=(10,4))
-    _ax.plot_date(x = _dates,y=_pVals, linestyle='-',marker=None,label=_cols[1],color='black')
+    #_ax.plot_date(x = _dates,y=_pVals, linestyle='-',marker=None,label=_cols[1],color='black')
+    _ax.plot_date(x = _dates,y=_pVals, fmt='-k',marker=None,label=_cols[1])
     for i in range(_nAsset):
-        _ax.plot_date(x = _dates, y = (_pVals[0]/_prices[0,i])*_prices[:,i],linestyle='-',marker=None,label=_cols[2*_nAsset+2+i])
+        _ax.plot_date(x = _dates, y = (_pVals[0]/_prices[0,i])*_prices[:,i],fmt='-',label=_cols[2*_nAsset+2+i])
+        #_ax.plot_date(x = _dates, y = (_pVals[0]/_prices[0,i])*_prices[:,i],linestyle='-',marker=None,label=_cols[2*_nAsset+2+i])
     _ax.legend(loc='upper left')
     _fig.autofmt_xdate(rotation=45)
     return _fig, _ax
